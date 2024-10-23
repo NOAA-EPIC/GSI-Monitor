f82165c961a4d89b77e32c11343f6f1bbddb2d0c
DavidBurrows-NCO <David.Burrows@noaa.gov>
2024-10-23 05:15:59 -0400

    Update scripts to build on Gaea-C5 and C6

--- modulefiles/gaeac5.intel-run.lua
+++ modulefiles/gaeac5.intel-run.lua
@@ -3,8 +3,8 @@ help([[
 
 prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/spack-stack-1.6.0/envs/unified-env/install/modulefiles/Core")
 
-local stack_intel_ver=os.getenv("stack_intel_ver") or "2023.1.0"
-local stack_cray_mpich_ver=os.getenv("stack_cray_mpich_ver") or "8.1.25"
+local stack_intel_ver=os.getenv("stack_intel_ver") or "2023.2.0"
+local stack_cray_mpich_ver=os.getenv("stack_cray_mpich_ver") or "8.1.28"
 local grads_ver=os.getenv("grads_ver") or "2.0.2"
 local prod_util_ver=os.getenv("prod_util_ver") or "2.1.1"
 
@@ -15,4 +15,4 @@ load(pathJoin("prod_util", prod_util_ver))
 
 load("common-run")
 
-whatis("Description: GSI Monitoring run-time environment on Hera.intel")
+whatis("Description: GSI Monitoring run-time environment on GaeaC5.intel")
